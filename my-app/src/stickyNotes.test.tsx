d293193f1ac59fcd7056f1e15e6cbf7cbdb8d11c
holin034 <linho111222@gmail.com>
2024-10-14 15:27:22 -0700

    2.6.1 updated

--- my-app/src/stickyNotes.test.tsx
+++ my-app/src/stickyNotes.test.tsx
@@ -30,4 +30,44 @@ describe("Create StickyNote", () => {
    expect(newNoteTitle).toBeInTheDocument();
    expect(newNoteContent).toBeInTheDocument();
  });
+ test ("Read: Are all the notes that are created displayed on the page", () => {
+    render(<StickyNotes />);
+    const testNoteOneTitle = screen.getByText("test note 1 title");
+    const testNoteTwoTitle = screen.getByText("test note 2 title");
+    const testNoteThreeTitle = screen.getByText("test note 3 title");
+    const testNoteFourTitle = screen.getByText("test note 4 title");
+    const testNoteFiveTitle = screen.getByText("test note 5 title");
+    const testNoteSixTitle = screen.getByText("test note 6 title");
+    const testNoteOneContent = screen.getByText("test note 1 content");
+    const testNoteTwoContent = screen.getByText("test note 2 content");
+    const testNoteThreeContent = screen.getByText("test note 3 content");
+    const testNoteFourContent = screen.getByText("test note 4 content");
+    const testNoteFiveContent = screen.getByText("test note 5 content");
+    const testNoteSixContent = screen.getByText("test note 6 content");
+    expect(testNoteOneTitle).toBeInTheDocument();
+    expect(testNoteTwoTitle).toBeInTheDocument();
+    expect(testNoteThreeTitle).toBeInTheDocument();
+    expect(testNoteFourTitle).toBeInTheDocument();
+    expect(testNoteFiveTitle).toBeInTheDocument();
+    expect(testNoteSixTitle).toBeInTheDocument();
+    expect(testNoteOneContent).toBeInTheDocument();
+    expect(testNoteTwoContent).toBeInTheDocument();
+    expect(testNoteThreeContent).toBeInTheDocument();
+    expect(testNoteFourContent).toBeInTheDocument();
+    expect(testNoteFiveContent).toBeInTheDocument();
+    expect(testNoteSixContent).toBeInTheDocument();
+ })
+
+ test ("Update: Once the update is done, is the document object value updating", () => {
+    render(<StickyNotes />);
+    
+    const testNoteOneContent = screen.getByText("test note 1 content");
+    
+    fireEvent.click(testNoteOneContent);
+    fireEvent.change(testNoteOneContent, { target: { textContent: "test note 1 something" } });
+    
+    const newTestNoteOneContent = screen.getByText("test note 1 something");
+    expect(newTestNoteOneContent).toBeInTheDocument();
+ })
+
 });
